e0af356590aabbccdff78d9aa03c93f3c606bfdc
changach-3000 <sharon.changach@student.moringaschool.com>
2023-05-27 11:33:45 +0300

    done with assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+('Mr. Chocolate', 5, 'M', 'Brown', 'Advocate', 1),
+('Rowdy', 7, 'M', 'Red', 'Mediator', 0),
+('Tabitha', 10, 'F', 'White', 'Melancholic', 1),
+('Sergeant Brown', 3, 'M', 'Beige', 'Choleric', 1),
+('Melissa', 2, 'F', 'Turquoise', 'Nostalgic', 0),
+('Grinch', 15, 'M', 'Grey', 'Advocate', 1),
+('Wendy', 5, 'F', 'Purple', 'Sanguine', 0),
+(NULL, 5, 'F', 'Yellow', 'Mediator', 1);
